d7d34b7a21318f306794b4c78a2ecd302d0f3df9
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-07-23 17:01:23 -0300

    wip: projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import AboutMe from "@/components/about-me/about-me";
-import Projects from "@/components/projects";
+import Projects from '@/components/projects/projects';
 import Skills from "@/components/skills/skills";
 
 export default function Home() {
