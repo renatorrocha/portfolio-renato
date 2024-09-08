366318cb15313e87fb6993affb7481cd298b4310
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-07 22:45:57 -0300

    fix: skills badges

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -10,7 +10,7 @@ export default function SkillsSection() {
         <BlurFade delay={BLUR_FADE_DELAY * 9}>
           <h2 className="text-xl font-bold">Skills</h2>
         </BlurFade>
-        <div className="flex flex-wrap gap-1">
+        <div className="flex flex-wrap justify-center gap-1">
           {DATA.skills.map((skill, id) => (
             <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
               <Badge key={skill} className="cursor-default">
