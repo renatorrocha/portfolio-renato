b72990463335f5ff4527df23ad0047c2154a8191
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-22 23:24:33 -0300

    docs: update Data

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,57 +8,6 @@ import { GiNotebook } from "react-icons/gi";
 
 export const BLUR_FADE_DELAY = 0.1;
 
-// export const MySkills: TechnologyCategory[] = [
-//   {
-//     name: "Languages",
-//     skills: [
-//       { Icon: SiJavascript, name: "Javascript" },
-//       { Icon: SiHtml5, name: "Html5" },
-//       { Icon: SiCss3, name: "Css3" },
-//       { Icon: SiTypescript, name: "Typescript" },
-//       { Icon: SiNodedotjs, name: "Node" },
-//       { Icon: FaJava, name: "Java" },
-//       { Icon: SiKotlin, name: "Kotlin" },
-//     ],
-//   },
-
-//   {
-//     name: "Libs",
-//     skills: [
-//       { Icon: SiReact, name: "React" },
-//       { Icon: SiStyledcomponents, name: "Styled Components" },
-//       { Icon: SiZod, name: "Zod" },
-//       { Icon: SiReacthookform, name: "Hook Form" },
-//       { Icon: SiShadcnui, name: "Shadcn-Ui" },
-//       { Icon: SiMui, name: "Mui" },
-//       { Icon: SiNextui, name: "Next-Ui" },
-//       { Icon: SiReactquery, name: "React Query" },
-//     ],
-//   },
-
-//   {
-//     name: "Frameworks",
-//     skills: [
-//       { Icon: SiTailwindcss, name: "Tailwind" },
-//       { Icon: SiVite, name: "Vite" },
-//       { Icon: SiNextdotjs, name: "Next" },
-//       { Icon: SiAstro, name: "Astro" },
-//       { Icon: SiTauri, name: "Tauri" },
-//     ],
-//   },
-
-//   {
-//     name: "Tools",
-//     skills: [
-//       { Icon: SiGit, name: "Git" },
-//       { Icon: SiNotion, name: "Notion" },
-//       { Icon: SiGithub, name: "Github" },
-//       { Icon: SiDocker, name: "Docker" },
-//       { Icon: BsAndroid, name: "Android Studio" },
-//     ],
-//   },
-// ];
-
 export const DATA = {
   name: "Renato Rocha Rodrigues",
   initials: "RR",
@@ -87,6 +36,8 @@ export const DATA = {
     "Kotlin",
     "Android",
     "Nginx",
+    "Bun",
+    "Elysia"
   ],
   navbar: [
     {
