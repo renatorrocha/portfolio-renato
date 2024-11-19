a34f7ce64e706c0d5d22e6912dd9232654a1ae70
renatorrocha <renatorrodrigues2002@gmail.com>
2024-11-19 00:18:24 -0300

    fix: sitemap

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,9 +5,19 @@ import HeroSection from "@/components/sections/hero";
 import ProjectsSection from "@/components/sections/projects";
 import SkillsSection from "@/components/sections/skills";
 import WorkSection from "@/components/sections/work";
+import Head from 'next/head';
 
 export default function Home() {
   return (
+    <>
+<Head>
+  <title>Renato Dev</title>
+  <meta
+          name="description"
+          content="Descrição clara e concisa sobre o conteúdo da página."
+        />
+</Head>
+
     <main className="flex min-h-[100dvh] flex-col space-y-10">
       <HeroSection />
 
@@ -23,5 +33,6 @@ export default function Home() {
 
       <ProjectsSection />
     </main>
+    </>
   );
 }
